bb47f50bdf40df6f2972da4fa22116f1168a5220
Kuchar <sz.blazynski@gmail.com>
2022-10-12 22:41:58 +0200

    Testing purpose

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 
 describe("Testing purpose", () => {
   it("3is3", () => {
-    const test = 3;
-    expect(3).toBe(test)
+    const testingPurpose = 3;
+    expect(3).toBe(testingPurpose);
   });
 });

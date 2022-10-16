0e8f0b63bd56ed7902dee60eace810a75128e6e4
Kuchar <sz.blazynski@gmail.com>
2022-10-16 16:46:29 +0200

    Configure firebase and husky with lint-staged

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,9 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-         Solar Plus App
-        </h1>
+        <h1 className={styles.title}>Solar Plus App</h1>
 
         <p className={styles.description}>
           Get started by editing{" "}

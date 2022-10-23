e0ddea68208fe86d65e99a6ad85f496bff4ebadc
Kuchar <sz.blazynski@gmail.com>
2022-10-23 21:08:49 +0200

    Create new farm with simple endopoints

---
+++ types/apiTypes.ts
@@ -0,0 +1,4 @@
+export type ResponseType = {
+  message: string;
+  success: boolean;
+};
---
+++ types/farm.ts
@@ -0,0 +1,22 @@
+export interface Farm {
+  id?: string;
+  farmName?: string;
+  location?: LocationCoordinates;
+  pvPanel?: PvPanelAttributes;
+}
+
+export type LocationCoordinates = {
+  latitude: string;
+  longitude: string;
+};
+
+export type PvPanelAttributes = {
+  peakPower: number;
+  loss: number;
+};
+
+export type FarmBodyModel = {
+  name: string;
+  location: LocationCoordinates;
+  pvPanel: PvPanelAttributes;
+};

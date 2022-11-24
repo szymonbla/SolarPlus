023b7a615d4cdc1950c207f498a44a2a55cd49a2
Kuchar <sz.blazynski@gmail.com>
2022-11-24 23:17:58 +0100

    Fix all minor things

--- pages/api/farm/[farmId].ts
+++ pages/api/farm/[farmId].ts
@@ -18,7 +18,7 @@ async function getFarmById(req: NextApiRequest, res: NextApiResponse) {
     const id = req.query && Number(req.query.farmId);
     const farmlooking = await prisma?.farm.findUnique({
       where: {
-        id: 0,
+        id,
       },
       include: { location: true, pvPanel: true },
     });
